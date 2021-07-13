190e985f88cb81cc4c5ef4f5ec1e7c89417b2c97
Samuel-Trujillo <samuel.trujillo093@gmail.com>
2021-07-12 17:53:15 -0700

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import Tab from './components/Tab';
 
 function App() {
+  // sets info within the clickable tags
+  const [tabs, setTabs] = useState(["hello this is tab 1","this is tab 2", "I like turtles"])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Tab tabs={tabs}/>
     </div>
   );
 }
